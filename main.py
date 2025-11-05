bc0fd72e37bc5e6081a711d558a7b8414106e84d
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2025-11-05 19:33:37 +1000

    remove and reconfigure uv

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from henryisonline!")
-
-
-if __name__ == "__main__":
-    main()
--- markdowntest.py
+++
@@ -1,5 +0,0 @@
-from pprint import pprint
-from markdown_it import MarkdownItimport 
-
-md = MarkdownIt()
-md.render("some *text*")
