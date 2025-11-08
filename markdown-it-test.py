81c9cf3b84a04c838042958446c4fdb6c57ff7a8
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2025-11-08 13:45:45 +1000

    begin testing plugins for markdown

--- markdown-it-test.py
+++ markdown-it-test.py
@@ -1,7 +1,28 @@
 from pprint import pprint
 from markdown_it import MarkdownIt
+from mdit_py_plugins.front_matter import front_matter_plugin
+from mdit_py_plugins.footnote import footnote_plugin
 
-md = MarkdownIt()
-output = md.render("example test. contains *italics* and **bold.**")
 
-print(output)
+md = (
+    MarkdownIt()
+    .use(front_matter_plugin)
+    .use(footnote_plugin)
+    .enable('table')
+)
+
+text = ("""
+        ---
+        a: 1
+        ---
+
+        a | b
+        - | -
+        1 | 2
+
+        A footnote [^1]
+
+        [^1]: some details
+        """)
+
+print(md.render(text))
