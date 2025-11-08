626f70741a85a1b23a426a60d82afb73ce55b669
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2025-11-08 10:49:02 +1000

    test successful

--- markdown-it-test.py
+++ markdown-it-test.py
@@ -2,4 +2,6 @@ from pprint import pprint
 from markdown_it import MarkdownIt
 
 md = MarkdownIt()
-md.render("example test. contains *italics* and **bold.**")
+output = md.render("example test. contains *italics* and **bold.**")
+
+print(output)
