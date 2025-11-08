1b606e1452e088cdfcc6cf4d04e1eb7accf91401
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2025-11-08 11:02:25 +1000

    add shell script to automate setup - under construction

---
+++ init-script.sh
@@ -0,0 +1,11 @@
+# shell script  future reference to make setup better
+# this script will (on a mac)
+# 1. install [homebrew](brew.sh)
+# 2. install python (`brew install python`)
+# 3. alias python in zsh to make your life easier (`alias python='python3'`)
+# 4. run `python -m venv .venv` - this will create a virtual environment in the project root
+# 5. activate the venv with `source .venv/bin/activate`
+#    - the terminal should now be prefixed with `(.venv)`
+# 6. run `pip install markdown-it-py pillow`
+
+echo "test - underconstruction"
