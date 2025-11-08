03f8ba62fc4cb1ecb02471876115c1ea8cc93541
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2025-11-08 16:16:52 +1000

    comment all lines for testing

--- markdown-it-test.py
+++ markdown-it-test.py
@@ -1,24 +1,24 @@
-from markdown_it import MarkdownIt
-from mdit_py_plugins.front_matter import front_matter_plugin
-from mdit_py_plugins.footnote import footnote_plugin
-
-md = (
-    MarkdownIt()
-    .use(front_matter_plugin)
-    .use(footnote_plugin)
-    .enable('table')
-)
-text = ("""\
----
-a: 1
----
-
-a | b
-- | -
-1 | 2
-
-A footnote [^1]
-
-[^1]: some details
-""")
-print(md.render(text))
+# from markdown_it import MarkdownIt
+# from mdit_py_plugins.front_matter import front_matter_plugin
+# from mdit_py_plugins.footnote import footnote_plugin
+# 
+# md = (
+#     MarkdownIt()
+#     .use(front_matter_plugin)
+#     .use(footnote_plugin)
+#     .enable('table')
+# )
+# text = ("""\
+# ---
+# a: 1
+# ---
+# 
+# a | b
+# - | -
+# 1 | 2
+# 
+# A footnote [^1]
+# 
+# [^1]: some details
+# """)
+# print(md.render(text))
